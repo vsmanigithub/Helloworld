48c2d036899e747ac61dbcd7e3edca46c4503931
EC2 Default User <ec2-user@ip-172-31-10-195.ap-south-1.compute.internal>
2023-11-27 15:19:27 +0000

    added lines

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+    System.out.println("this is madhu");
+    System.out.println("Hello, Banglore");
+    System.out.println("Hello, canada");
+    System.out.println("Hello, kadapa ");
+    System.out.println("Hello, wed......................");
+  }
+}
